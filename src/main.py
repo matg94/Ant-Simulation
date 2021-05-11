1f566d19e7beb36c35951bc70bbe962b064f1108
matg94 <mat.gautron@gmail.com>
2021-05-11 18:44:02 +0100

    setting up main.py and git

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode([900, 600])
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((255,255,255))
+
+
+        pygame.display.flip()
